5823eeb07d33a5db67b63542e6ba2b0af0b3c551
jofetta <jafarovatatiana@gmail.com>
2024-03-07 22:12:23 +0300

    feat: solve sortCities

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -253,8 +253,23 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  const sortedArr = arr.sort((a, b) => {
+    if (a.country < b.country) {
+      return -1;
+    }
+    if (a.country === b.country) {
+      if (a.city < b.city) {
+        return -1;
+      }
+      return 1;
+    }
+    if (a.country > b.country) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedArr;
 }
 
 /**
